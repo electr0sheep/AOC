e040a1b70300270d1e1b7c5c87bc6ca4e386ec2b
Michael DeGraw <michael.degraw@flybreeze.com>
2024-12-09 16:06:16 -0700

    day7

---
+++ 2024/day7/part2.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def concatenate(num1, num2)
+  "#{num1}#{num2}".to_i
+end
+
+def possible?(scenario, index, sum)
+  return false if sum < scenario[:test_value] && scenario[:numbers][index].nil?
+  return false if sum > scenario[:test_value]
+  return true if sum == scenario[:test_value] && scenario[:numbers][index].nil?
+
+  possible?(scenario, index + 1, sum + scenario[:numbers][index]) ||
+    possible?(scenario, index + 1, sum * scenario[:numbers][index]) ||
+    possible?(scenario, index + 1, concatenate(sum, scenario[:numbers][index]))
+end
+
+result = 0
+input = File.read(File.join(File.dirname(__FILE__), 'input.txt'))
+lines = input.split("\n")
+scenarios = lines.map { |l| { test_value: l.split(': ')[0].to_i, numbers: l.split(': ')[1].split(' ').map(&:to_i) } }
+
+scenarios.each do |scenario|
+  result += scenario[:test_value] if possible?(scenario, 1, scenario[:numbers][0])
+end
+
+puts result
